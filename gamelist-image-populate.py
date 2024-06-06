d0bee4f7da38913a604e0a366a750d828b98fa5e
Pablo <pablo_penna7@hotmail.com>
2024-06-06 20:38:43 +0100

    Search for images matching game name and add to xml

--- gamelist-image-populate.py
+++ gamelist-image-populate.py
@@ -3,8 +3,9 @@ import argparse
 import sys
 import os
 import shutil
+from pathlib import Path
 
-def initParameterParser() -> argparse.ArgumentParser:
+def _initParameterParser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog=sys.argv[0],
         description='What the program does',
@@ -23,49 +24,98 @@ def initParameterParser() -> argparse.ArgumentParser:
     )
     return parser
 
-def getTargetFileFromArguments(folderPath: str, fileName: str):
+def _getTargetFileFromArguments(folderPath: str, fileName: str):
     return os.path.join(folderPath, fileName)
 
-def backupOriginalFile(filepath: str):
+def _backupOriginalFile(filepath: str):
     shutil.copy2(filepath, filepath+".bak")
 
-def openGamelistAsXml(filepath: str):
+def _openGamelistAsXml(filepath: str):
     return ElementTree.parse(filepath)
 
-def getGameListFromXmlRoot(root: ElementTree.Element):
+def _getGameListFromXmlRoot(root: ElementTree.Element):
     return root.iter("game")
 
-def addImageToGameEntry(gameEntry: ElementTree.Element):
-    image = ElementTree.SubElement(gameEntry, "image")
-    image.text = "placeholder"
-    
+def _getNameFromGameEntry(gameEntry: ElementTree.Element):
+    return gameEntry.find("name").text
+
+def _getPathFromGameEntry(gameEntry: ElementTree.Element):
+    return gameEntry.find("path").text
+
+def _getFilenameFromGameEntry(gameEntry: ElementTree.Element):
+    gamePath = Path(_getPathFromGameEntry(gameEntry))
+    return gamePath.stem
+
+def _getImageFromGameEntry(gameEntry: ElementTree.Element):
+    return gameEntry.find("image")
+
+def _doesGameEntryHasImageTag(gameEntry: ElementTree.Element):
+    return _getImageFromGameEntry(gameEntry) != None
+
+def _processGameEntry(baseFolder:str, gameEntry: ElementTree.Element):
+    if _doesGameEntryHasImageTag(gameEntry):
+        return
+    _addImageToGameEntry(baseFolder, gameEntry)
+
+def _addImageToGameEntry(baseFolder:str, gameEntry: ElementTree.Element):
+    imagePath = _getImagePathForGame(baseFolder, gameEntry)
+    if imagePath != None:
+        image = ElementTree.SubElement(gameEntry, "image")
+        image.text = imagePath
+
+def _getImagePathForGame(baseFolder:str, gameEntry: ElementTree.Element):
+    imageFilePath = _getFilepathForImageMatchingGameFilename(
+        baseFolder, 
+        gameEntry
+    ) or _getFilepathForImageMatchingGameName(
+        baseFolder, 
+        gameEntry
+    )
+    if imageFilePath == None:
+        print("Could not find existing image for {}".format(_getNameFromGameEntry(gameEntry)))
+    else:
+        print("Found image at {} for {}".format(imageFilePath, _getNameFromGameEntry(gameEntry)))
+        imageFilePath = _makeFilepathRelative(imageFilePath, baseFolder)
+        print("Transformed to relative path: {}".format(imageFilePath))
+    return imageFilePath
+
+def _getFilepathForImageMatchingGameName(baseFolder:str, gameEntry: ElementTree.Element):
+    return _getFilepathForImageWithName(baseFolder, _getNameFromGameEntry(gameEntry))
+
+def _getFilepathForImageMatchingGameFilename(baseFolder:str, gameEntry: ElementTree.Element):
+    return _getFilepathForImageWithName(baseFolder, _getFilenameFromGameEntry(gameEntry))
+
+def _getFilepathForImageWithName(baseFolder:str, name: str):
+    extension = ".png"
+    imagesFolderName = "images"
+    imagePath = os.path.join(baseFolder, imagesFolderName, name + extension)
+    print("Trying path {}".format(imagePath))
+    return imagePath if Path(imagePath).exists() else None
+
+def _makeFilepathRelative(filepath: str, baseFolder: str):
+    return "./{}".format(os.path.relpath(filepath, baseFolder))
+
+def _removeImageFromGameEntry(gameEntry: ElementTree.Element):
+    imageTag = _getImageFromGameEntry(gameEntry)
+    gameEntry.remove(imageTag)
 
 if __name__ == "__main__":
-    print("hello world")
-
-    argParser = initParameterParser()
+    argParser = _initParameterParser()
     args = argParser.parse_args()
     
-    filepath = getTargetFileFromArguments(args.folder, args.gamelist_file)
-    backupOriginalFile(filepath)
+    baseFolder = os.path.join(os.getcwd(), args.folder)
+    filepath = _getTargetFileFromArguments(args.folder, args.gamelist_file)
+    _backupOriginalFile(filepath)
 
-    gamelistFile = openGamelistAsXml(filepath)
-    root = gamelistFile.getroot()
+    gamelistXml = _openGamelistAsXml(filepath)
+    root = gamelistXml.getroot()
     print(root)
 
-    for game in getGameListFromXmlRoot(root):
-        print("====GAME====")
-        print("tag: ")
-        print(game.tag)
-        print("attrib: ")
-        print(game.attrib)
-        addImageToGameEntry(game)
-        for prop in game:
-            print(prop.tag)
-            print(prop.attrib)
-            print(prop.text)
+    for game in _getGameListFromXmlRoot(root):
+        _processGameEntry(baseFolder, game)
+        #_removeImageFromGameEntry(game)
 
-    gamelistFile.write(filepath)
+    gamelistXml.write(filepath)
 
 
 
